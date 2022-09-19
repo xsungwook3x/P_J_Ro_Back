754ec6f80f85490b832bdb9286d48f9a7316266e
Seonguk <xsungwook3x@naver.com>
2022-09-19 22:46:30 +0900

    add : pjro backend setting

---
+++ PJRO/src/main/java/com/ugi/PJRO/PjroApplication.java
@@ -0,0 +1,13 @@
+package com.ugi.PJRO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjroApplication.class, args);
+	}
+
+}
---
+++ PJRO/src/test/java/com/ugi/PJRO/PjroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugi.PJRO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
